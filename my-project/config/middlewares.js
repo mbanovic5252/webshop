4a4f259ce06b457c420bc4f9e53f764ee0ed58db
mbanovic5252 <warlord5252@gmail.com>
2023-04-05 17:37:34 +0200

    added more origins

--- my-project/config/middlewares.js
+++ my-project/config/middlewares.js
@@ -30,7 +30,11 @@ module.exports = [
   {
     name: "strapi::cors",
     config: {
-      origin: ["https://webshop-one-umber.vercel.app/"],
+      origin: [
+        "https://webshop-one-umber.vercel.app/",
+        "https://webshop-production-4b16.up.railway.app",
+        "containers-us-west-139.railway.app",
+      ],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
       keepHeaderOnError: true,
