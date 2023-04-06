2df41fbd5e80e1a93c01adc757b00be11d188da3
mbanovic5252 <warlord5252@gmail.com>
2023-04-06 19:59:51 +0200

    fix item details

--- frontend/src/scenes/ItemDetails/ItemDetails.jsx
+++ frontend/src/scenes/ItemDetails/ItemDetails.jsx
@@ -59,7 +59,7 @@ const ItemDetails = () => {
             alt={item?.name}
             width="100%"
             height="100%"
-            src={`https://webshop-production-4b16.up.railway.app${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
+            src={`${item?.attributes?.image?.data?.attributes?.formats?.medium?.url}`}
             style={{ objectFit: "contain" }}
           />
         </Box>
