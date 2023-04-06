e886eb8a598c394e1a8047940a2c7762f372fa73
mbanovic5252 <warlord5252@gmail.com>
2023-04-06 19:27:42 +0200

    order fix

--- my-project/src/api/order/controllers/order.js
+++ my-project/src/api/order/controllers/order.js
@@ -35,8 +35,8 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         payment_method_types: ["card"],
         customer_email: email,
         mode: "payment",
-        success_url: "http://localhost:5173/checkout/success",
-        cancel_url: "http://localhost:5173",
+        success_url: "/checkout/success",
+        cancel_url: "/",
         line_items: lineItems,
       });
 
